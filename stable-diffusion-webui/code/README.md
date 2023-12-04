e273232cc4916d72228521b591f1cb9a48e0bd11
John Shaughnessy <john@johnshaughnessy.com>
2023-12-04 13:39:48 -0500

    Move AUTOMATIC1111/stable-diffusion-webui to osai-apps

--- stable-diffusion-webui/code/README.md
+++
@@ -1,1 +0,0 @@
-This directory can contain a cloned git repo on the target node, but will be ignored in the osai-ubuntu project.
--- stable-diffusion-webui/container_init.sh
+++
@@ -1,6 +0,0 @@
-wget -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh
-chmod +x webui.sh
-
-echo 'print("Done installing!");' >print_done.py
-
-LAUNCH_SCRIPT="../print_done.py" ./webui.sh
